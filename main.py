4d784838cc4716c2f284292c03bb1a380740d48f
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2025-04-05 01:54:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def unsubscribe_account_verification_code(email_unsubscribe: str) -> None:
             
 # Display logo
 st.image('logo.png')
-tab1, tab2, tab3 = st.tabs(['Register', 'Unsubscribe', 'Stats', 'Contact'])
+tab1, tab2, tab3, tab4 = st.tabs(['Register', 'Unsubscribe', 'Stats', 'Contact'])
 
 # Registration tab
 with tab1:
