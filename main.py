e7aed2f74ea314e29acc6e9df7c975f3eee4ee61
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2025-04-14 23:27:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,99 +84,120 @@ def unsubscribe_account_verification_code(email_unsubscribe: str) -> None:
             
 # Display logo
 st.image('logo.png')
-tab1, tab2, tab3, tab4 = st.tabs(['Register', 'Unsubscribe', 'Stats', 'Contact'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Register', 'Unsubscribe', 'Stats', 'Contact', 'Feedback'])
 
-# Registration tab
-with tab1:
-    st.markdown("""Register to receive a free API key to use Streamlit Authenticator's
-        **two factor authentication** and **send email** features""")
-    app_name: str = st.text_input('Your application name', autocomplete='off')
-    email_register: str = st.text_input('Email', key='email_register', autocomplete='off')
-    
-    if 'register_code' not in st.session_state:
-        st.session_state['register_code'] = None
-
-    if st.button('Register'):
-        try:
-            if not validate_email(email_register):
-                raise ValueError('Email is not valid')
-            if not validate_length(app_name):
-                raise ValueError('Application name is not valid')
-            result = email_previously_registered(email_register)
-            if 'not previously registered' in result['message']:
-                register_code: str = generate_random_verification_code()
-                st.session_state['register_code'] = hash(register_code)
-                send_email_general('Streamlit Authenticator Verification Code',
-                                   register_code, email_register, '2FA')
-            else:
-                raise ValueError('Email is already registered')
-        except ValueError as e:
-            st.error(str(e))
-    
-    if st.session_state['register_code'] is not None:
-        register_verification_code(app_name, email_register)
-
-# Unsubscribe tab
-with tab2:
-    st.markdown("""Use the form below to unsubscribe and delete your account""")
-    email_unsubscribe: str = st.text_input('Email', key='email_unsubscribe', autocomplete='off')
-    
-    if 'unsubscribe_code' not in st.session_state:
-        st.session_state['unsubscribe_code'] = None
-
-    if st.button('Unsubscribe'):
-        try:
-            if not validate_email(email_unsubscribe):
-                raise ValueError('Email is not valid')
-            result = email_previously_registered(email_unsubscribe)
-            if 'not previously registered' in result['message']:
-                raise ValueError('An account with this email does not exist')
-            unsubscribe_code: str = generate_random_verification_code()
-            st.session_state['unsubscribe_code'] = hash(unsubscribe_code)
-            send_email_general('Streamlit Authenticator Verification Code',
-                               unsubscribe_code, email_unsubscribe, '2FA')
-        except ValueError as e:
-            st.error(str(e))
-    
-    if st.session_state['unsubscribe_code'] is not None:
-        unsubscribe_account_verification_code(email_unsubscribe)
+if not st.query_params['email']:
+    # Registration tab
+    with tab1:
+        st.markdown("""Register to receive a free API key to use Streamlit Authenticator's
+            **two factor authentication** and **send email** features""")
+        app_name: str = st.text_input('Your application name', autocomplete='off')
+        email_register: str = st.text_input('Email', key='email_register', autocomplete='off')
         
-# Stats tab
-with tab3:
-    st.markdown("""Use the form below to retrieve the number of times your users have used the **two factor authentication** and/or **send email** features""")
-    api_key: str = st.text_input('API key', key='api_key', autocomplete='off')
-    if st.button('Check'):
-        try:
-            if not validate_length(api_key, min_length=32, max_length=32):
-                raise ValueError('API key is not correct')
-            result = count_calls(api_key)
-            if 'None' in result['message']:
-                raise ValueError('An account with this API key does not exist')
-            result = json.loads(result['message'].replace("'", '"'))
-            result = pd.DataFrame.from_dict(result, orient='index', columns=['Calls'])
-            st.write('___')
-            st.metric('Total number of calls', int(result['Calls'].sum()))
-            if int(result['Calls'].sum()) > 0:
-                st.bar_chart(result)
-        except ValueError as e:
-            st.error(str(e))
+        if 'register_code' not in st.session_state:
+            st.session_state['register_code'] = None
 
-# Contact tab
-with tab4:
-    st.markdown("""Use the form below to contact us for any queries""")
-    sender_email = st.text_input('Email')
-    contact_message = st.text_area('Message')
-    if st.button('Send'):
-        try:
-            if not validate_email(sender_email):
-                raise ValueError('Email is not valid')
-            if not validate_length(contact_message):
-                raise ValueError('Message cannot be empty')
-            send_email_general('Streamlit Authenticator Contact',
-                           contact_message, sender_email, 'CONTACT')
-            st.success('Message sent successfully')
-        except ValueError as e:
-            st.error(str(e))
+        if st.button('Register'):
+            try:
+                if not validate_email(email_register):
+                    raise ValueError('Email is not valid')
+                if not validate_length(app_name):
+                    raise ValueError('Application name is not valid')
+                result = email_previously_registered(email_register)
+                if 'not previously registered' in result['message']:
+                    register_code: str = generate_random_verification_code()
+                    st.session_state['register_code'] = hash(register_code)
+                    send_email_general('Streamlit Authenticator Verification Code',
+                                    register_code, email_register, '2FA')
+                else:
+                    raise ValueError('Email is already registered')
+            except ValueError as e:
+                st.error(str(e))
+        
+        if st.session_state['register_code'] is not None:
+            register_verification_code(app_name, email_register)
+
+    # Unsubscribe tab
+    with tab2:
+        st.markdown("""Use the form below to unsubscribe and delete your account""")
+        email_unsubscribe: str = st.text_input('Email', key='email_unsubscribe', autocomplete='off')
+        
+        if 'unsubscribe_code' not in st.session_state:
+            st.session_state['unsubscribe_code'] = None
+
+        if st.button('Unsubscribe'):
+            try:
+                if not validate_email(email_unsubscribe):
+                    raise ValueError('Email is not valid')
+                result = email_previously_registered(email_unsubscribe)
+                if 'not previously registered' in result['message']:
+                    raise ValueError('An account with this email does not exist')
+                unsubscribe_code: str = generate_random_verification_code()
+                st.session_state['unsubscribe_code'] = hash(unsubscribe_code)
+                send_email_general('Streamlit Authenticator Verification Code',
+                                unsubscribe_code, email_unsubscribe, '2FA')
+            except ValueError as e:
+                st.error(str(e))
+        
+        if st.session_state['unsubscribe_code'] is not None:
+            unsubscribe_account_verification_code(email_unsubscribe)
+            
+    # Stats tab
+    with tab3:
+        st.markdown("""Use the form below to retrieve the number of times your users have used the **two factor authentication** and/or **send email** features""")
+        api_key: str = st.text_input('API key', key='api_key', autocomplete='off')
+        if st.button('Check'):
+            try:
+                if not validate_length(api_key, min_length=32, max_length=32):
+                    raise ValueError('API key is not correct')
+                result = count_calls(api_key)
+                if 'None' in result['message']:
+                    raise ValueError('An account with this API key does not exist')
+                result = json.loads(result['message'].replace("'", '"'))
+                result = pd.DataFrame.from_dict(result, orient='index', columns=['Calls'])
+                st.write('___')
+                st.metric('Total number of calls', int(result['Calls'].sum()))
+                if int(result['Calls'].sum()) > 0:
+                    st.bar_chart(result)
+            except ValueError as e:
+                st.error(str(e))
+
+    # Contact tab
+    with tab4:
+        st.markdown("""Use the form below to contact us for any queries""")
+        sender_email = st.text_input('Email')
+        contact_message = st.text_area('Message')
+        if st.button('Send'):
+            try:
+                if not validate_email(sender_email):
+                    raise ValueError('Email is not valid')
+                if not validate_length(contact_message):
+                    raise ValueError('Message cannot be empty')
+                send_email_general('Streamlit Authenticator Contact',
+                            contact_message, sender_email, 'CONTACT')
+                st.success('Message sent successfully')
+            except ValueError as e:
+                st.error(str(e))
+
+# Feedback tab
+if st.query_params.get('email'):
+    with tab5:
+        st.markdown("""Use the form below to provide us with feedback for your subscription to Streamlit Authenticator""")
+        q0 = st.text_input('Email', value=st.query_params['email'], disabled=True)
+        q1 = st.checkbox("""Would you be willing to pay to use Streamlit Authenticator's two factor authentication and
+                         send email features?""")
+        q2 = st.radio('If yes which one of the following payment models would your prefer?', 
+                      ['Monthly subscription', 'Annual', 'Lifetime'])
+        q3 = st.number_input(f'How much would you be willing to pay for the **{q2}** payment method in USD?', [0, 100])
+        q4 = st.text_area('Any additional feedback?')
+        if st.button('Send'):
+            try:
+                send_email_general('Streamlit Authenticator Feedback',
+                            str(f'email: {q0}, pay: {q1}, method: {q2}, amount: {q3}, message: {q4}'),
+                            'noreply@stauthenticator.com', 'CONTACT')
+                st.success('Message sent successfully')
+            except ValueError as e:
+                st.error(str(e))
 
 # Footer
 st.write('___')
